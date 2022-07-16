2f32711112b2fbcb7fd2771ede1aba6ab5f6d92d
randika-chathuranga <randikachathuranga219@gmail.com>
2022-07-16 22:00:39 +0530

    first page finished

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,17 +9,27 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent implements OnInit {
 
   trendingMovies: any;
+  theatreMovies: any;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getTrendingMovies();
+    this.getTheatreMovies();
   }
 
   getTrendingMovies(){
     this.http.get('http://localhost:4200/assets/data/trending-movies.json').subscribe((movies)=>{
       this.trendingMovies = movies;
       console.log(this.trendingMovies);
+    })
+  }
+
+
+  getTheatreMovies(){
+    this.http.get('http://localhost:4200/assets/data/theatre-movies.json').subscribe((movies)=>{
+      this.theatreMovies = movies;
+      console.log(this.theatreMovies);
     })
   }
 
